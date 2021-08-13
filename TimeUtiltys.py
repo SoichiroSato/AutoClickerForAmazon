930fa839ac48966cafa2cd5f68f42b7db8a08c5a
SoichiroSato <36285803+SoichiroSato@users.noreply.github.com>
2021-08-13 22:52:59 +0900

    各処理をオブジェクト化 エラー処理を修正

---
+++ TimeUtiltys.py
@@ -0,0 +1,18 @@
+import NTPClient
+import datetime
+from time import perf_counter
+import datetime
+
+class TimeUtiltys():
+
+    #指定時間まで待機させる
+    def MakeSleep(weitSec :float):
+        until = perf_counter() + weitSec
+        while perf_counter() < until:
+            pass
+
+    #Ntpサーバーの時間と指定時間の差を求める
+    def FindTheTimeDifference(ts :datetime ,ntpClient :NTPClient):
+        return ts.timestamp() - ntpClient.GetNowTime()
+
+    
